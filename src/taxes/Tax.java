5026c6dbab0e294ffff9f8daf25cb29411d578ea
Thayonara <tpa@cin.ufpe.br>
2022-06-02 19:10:57 -0300

    PATCH calculate method

--- src/taxes/Tax.java
+++ src/taxes/Tax.java
@@ -14,6 +14,10 @@ public abstract class Tax {
         if(mustApply(product)){
             doCalculation(product);
         }
+        if (next != null){
+            next.calculate(product);
+        }
+
         return product;
     }
 
