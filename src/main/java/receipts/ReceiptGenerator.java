35b764978475b6d85bec819b6bc87ae28b2f40f5
Thayonara <tpa@cin.ufpe.br>
2022-06-19 22:37:37 -0300

    requested adjustments

--- src/main/java/receipts/ReceiptGenerator.java
+++ src/main/java/receipts/ReceiptGenerator.java
@@ -24,7 +24,7 @@ public class ReceiptGenerator {
 
             //round only when the product was taxed
             if (s.getProduct().getValueAfterTaxes().subtract(s.getProduct().getValue()).multiply(s.getQuantity()).doubleValue() != 0){
-                s.getProduct().setValueAfterTaxes(s.getProduct().getValue().add(new RoundFiveCents().roundFiveCents((s.getProduct().getValueAfterTaxes()).subtract(s.getProduct().getValue()).multiply(s.getQuantity()))));
+                s.getProduct().setValueAfterTaxes(s.getProduct().getValue().add(new RoundFiveCents().roundFiveCents((s.getProduct().getValueAfterTaxes()).subtract(s.getProduct().getValue()))));
             }
             tax = tax.add(((s.getProduct().getValueAfterTaxes()).subtract(s.getProduct().getValue())).multiply(s.getQuantity()));
             total = total.add(s.getProduct().getValue().add((s.getProduct().getValueAfterTaxes()).subtract(s.getProduct().getValue())).multiply(s.getQuantity()));
